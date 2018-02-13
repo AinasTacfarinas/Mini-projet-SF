b565e3b22e9e73e87dc070265bbe29d06747ffc7
Tac <ainas.tacfarinas@hotmail.fr>
2018-02-13 20:01:36 +0100

    question6

--- src/Project/MainClass.java
+++ src/Project/MainClass.java
@@ -1,43 +1,54 @@
 package Project;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.util.concurrent.TimeUnit;
 public class MainClass {
 
-	final static int N = 2;
-	final static int K = 8;
-	static char []initTable;
-	static char []endTable;
-	static int k;
-	
-	
+	final static int N = 3;
+	static int K = 0;
+	static int k=0;
+
+
 	static int DEBUG = 10;
 
-	public static void initGame() {
-		initTable = new char[2*N+1]	;
-		int i;	
-		for(i=0;i<N;i++) {	
-			initTable[i]='V';
-		}
-		initTable[i]='N';
-		for(int j=i+1;j<2*N+1;j++) {
-			initTable[j]='R';
-		}
-
+	
+	public static String toS(String s, int i, int j) {
+		return ""+s+"_"+i+"_"+j;
 	}
-
-	public static void endGame() {
-		endTable = new char[2*N+1];
-		int i;
+	
+	public static String initState() {
+		String res = "";
+		int i=0;
 		for(i=0;i<N;i++) {
-			endTable[i]='R';
+			res+="("+toS("V",i,0)+" & "+ toS("!R",i,0)+") & ";
 		}
-		endTable[i]='N';
+		
+		res+="("+toS("!V",i,0)+" & "+ toS("!R",i,0)+")";
+		
 		for(int j=i+1;j<2*N+1;j++) {
-			endTable[j]='V';
+			res+=" & ("+toS("!V",j,0)+" & "+ toS("R",j,0)+")";
 		}
-
+		return res;
 	}
-
-
+	
+	
+	public static String finalState() {
+		String res = "";
+		int i=0;
+		for(i=0;i<N;i++) {
+			res+="("+toS("!V",i,k)+" & "+ toS("R",i,k)+") & ";
+		}
+		
+		res+="("+toS("!V",i,k)+" & "+ toS("!R",i,k)+") ";
+		
+		for(int j=i;j<2*N+1;j++) {
+			res+="("+toS("V",j,k)+" & "+ toS("!R",j,k)+") & ";
+		}
+		return res;
+	}
 
 
 	public static String printStoneState(char[]t,int stoneindex, int time) {
@@ -60,19 +71,19 @@ public class MainClass {
 		return res;
 	}
 
-	public static void printTableInBooleanForm(char[]t) {
+	public static String printTableInBooleanForm(char[]t) {
+		String res= "";
 		for(int i=0;i<2*N+1;i++) {
-			System.out.print(printStoneState(t,i,k));
+			res+=printStoneState(t,i,k);
 			if(i!=2*N) {
-				System.out.print(" & ");	
+				res+= " & ";	
 			}
 		}
-		System.out.println();
+
+		return res+="\n";
 	}
 
-	public static String toS(String s, int i, int j) {
-		return ""+s+"_"+i+"_"+j;
-	}
+	
 
 	public static String move1FromLeft(int j) {
 
@@ -82,9 +93,9 @@ public class MainClass {
 			res+="("+toS("V",i,k+1)+" = "+toS("V",i,k)+") & ("+toS("R",i,k+1)+" = "+toS("R",i,k)+")";
 
 			res+=" & ";
-			
+
 		}
-		
+
 		res +=  "\n"+
 				"("+toS("V",j+1,k+1)+" & (("+toS("V",j,k)+" & "+toS("!R",j,k)+") & ("+toS("!V",j+1,k)+" & "+toS("!R",j+1,k)+"))) & \n" + 
 				"("+toS("R",j+1,k+1)+" = "+toS("!V",j+1,k+1)+") & \n" + 
@@ -107,7 +118,7 @@ public class MainClass {
 			res+="("+toS("V",i,k+1)+" = "+toS("V",i,k)+") & ("+toS("R",i,k+1)+" = "+toS("R",i,k)+")";
 
 			res+=" & ";
-			
+
 		}
 
 		res += "\n"+
@@ -132,7 +143,7 @@ public class MainClass {
 			res+="("+toS("V",i,k+1)+" = "+toS("V",i,k)+") & ("+toS("R",i,k+1)+" = "+toS("R",i,k)+")";
 
 			res+=" & ";
-			
+
 		}
 
 		res +=  "\n"+
@@ -140,8 +151,8 @@ public class MainClass {
 				"("+toS("R",j+2,k+1)+" = "+toS("!V",j+2,k+1)+") & \n" + 
 				"("+toS("R",j,k+1)+" = "+toS("R",j+2,k)+") & ("+toS("V",j,k+1)+" = "+toS("V",j+2,k)+") &\n"+
 				"("+toS("V",j,k)+" = "+toS("!V",j+1,k)+") & ("+toS("R",j,k)+" = "+toS("!R",j+1,k)+")\n";
-		
-		
+
+
 		for(int i=j+3;i<2*N+1;i++) {
 			res+="& ("+toS("V",i,k+1)+" = "+toS("V",i,k)+") & ("+toS("R",i,k+1)+" = "+toS("R",i,k)+")";
 		}
@@ -159,7 +170,7 @@ public class MainClass {
 		for(int i=2*N;i>j;i--) {
 			res+="("+toS("V",i,k+1)+" = "+toS("V",i,k)+") & ("+toS("R",i,k+1)+" = "+toS("R",i,k)+")";
 			res+=" & ";
-			
+
 		}
 
 		res += "\n"+
@@ -177,28 +188,28 @@ public class MainClass {
 
 		return res+"\n)";
 	}
-	
+
 	public static String question3_4() {
 		String res = "!(";
-		
+
 		for(int i=0;i<2*N+1;i++) {
 			res+="("+toS("V",i,k)+" & "+toS("R",i,k)+")";
-			
+
 			if(i<2*N) {
 				res+=" | ";	
 			}
-			
+
 		}
 		res+=")";
-		
+
 		return res;
 	}
-	
+
 	public static String question5() {
 		String res = "!(\n";
-		
+
 		for(int i=0;i<=k;i++) {
-			
+
 			for(int j=i+1;j<=k;j++) {
 				res+="(";
 				for(int m=0;m<2*N+1;m++) {
@@ -211,99 +222,152 @@ public class MainClass {
 				if(i<k-1) {
 					res+="\n|\n";
 				}
-				
-			}
-			
-			
+
+			}	
+
+
 		}
 		res+="\n)";
-		
+
 		return res;
 	}
 
 
-	public static int execute() {
-		printTableInBooleanForm(initTable);
+	public static int execute() throws Exception {
+
+		String booltocnb = "bool2cnf-20110304/a.out";
+		String minisat = "minisat/build/release/bin/minisat";
+		String file1 = "test.txt";
+		String file2 = "test.cnf";
+		String file3 = "test.out";
 		
-		while(k<K) {
-			boolean writen = false;
-			System.out.println("\n&(\n");
-			for(int i=0;i<2*N+1;i++) {
-				
-				
-
-				if(i>0) {
-					if(writen) {
-						System.out.println("\n|\n");
-					}else {
-						writen= true;
-					}
-					if(DEBUG<5) {
-						System.out.println("1 from right "+i);
-					}
-					
-					System.out.println(move1FromRight(i));
-					
-				}
-				
-
-				if(i>1) {
-					if(writen) {
-						System.out.println("\n|\n");
-					}else {
-						writen= true;
-					}
-					if(DEBUG<5) {
-						System.out.println("2 from right "+i);
-					}
-					System.out.println(move2FromRight(i));
-				}
-				
-
-				if(i<2*N) {
-					if(writen) {
-						System.out.println("\n|\n ");
-					}else {
-						writen= true;
-					}
-					if(DEBUG<5) {
-						System.out.println("1 from left "+i);
-					}
-					System.out.println(move1FromLeft(i));
-				}
-				
-				if(i<2*N-1) {
-					if(writen) {
-						System.out.println("\n|\n");
-					}else {
-						writen= true;
-					}				
-					if(DEBUG<5) {
-						System.out.println("2 from left "+i);
-					}
-					System.out.println(move2FromLeft(i));
-				}
-
-			}
+		String blast="";
+		while(!blast.equals("SATISFIABLE")) {
+			String res=initState();
+			k=0;
+			while(k<=K) {			
 			
-			System.out.println("\n)\n&\n"+question3_4()+"\n");
-			k++;
+			boolean writen = false;
+			res+="\n&(\n";
+			
+			
+				for(int i=0;i<2*N+1;i++) {
+
+					if(i>0) {
+						if(writen) {
+							res+="\n|\n";
+						}else {
+							writen= true;
+						}
+						if(DEBUG<5) {
+							System.out.println("1 from right "+i);
+						}
+
+						res+=move1FromRight(i);
+
+					}
+
+
+					if(i>1) {
+						if(writen) {
+							res+="\n|\n";
+						}else {
+							writen= true;
+						}
+						if(DEBUG<5) {
+							System.out.println("2 from right "+i);
+						}
+						res+=move2FromRight(i);
+					}
+
+
+					if(i<2*N) {
+						if(writen) {
+							res+="\n|\n";
+						}else {
+							writen= true;
+						}
+						if(DEBUG<5) {
+							System.out.println("1 from left "+i);
+						}
+						res+=move1FromLeft(i);
+					}
+
+					if(i<2*N-1) {
+						if(writen) {
+							res+="\n|\n";
+						}else {
+							writen= true;
+						}				
+						if(DEBUG<5) {
+							System.out.println("2 from left "+i);
+						}
+						res+=move2FromLeft(i);
+					}
+
+				}
+				res+="\n)\n&\n"+question3_4()+"\n";
+				k++;
+				
+			}
+			res+="\n&\n"+question5()+"\n\n\n&"+finalState();
+			System.out.println(res);
+			File f;
+			FileOutputStream fos;
+			f = new File(file1);
+			fos = new FileOutputStream(f);
+			fos.write(res.getBytes());
+			K++;
+
+			Process p = new ProcessBuilder()
+					.command(booltocnb)
+					.redirectInput(new File(file1))
+					.redirectOutput(new File(file2))
+					.start();
+
+			boolean finished = p.waitFor(5, TimeUnit.SECONDS);
+			if (!finished) {
+				p.destroy();
+				finished=p.waitFor(5, TimeUnit.SECONDS); // wait for the process to terminate
+			}
+
+			Process p2 = new ProcessBuilder()
+					.command(minisat)
+					.redirectInput(new File(file2))
+					.redirectOutput(new File(file3))
+					.start();
+
+
+			boolean finished2 = p2.waitFor(5, TimeUnit.SECONDS);
+			if (!finished2) {
+				p2.destroy();
+				finished2=p.waitFor(5, TimeUnit.SECONDS); // wait for the process to terminate
+			}
+
+			BufferedReader bis = new BufferedReader(new FileReader(file3));
+
+
+			String last="";
+			last = bis.readLine();
+
+			while((last = bis.readLine())!=null) {
+				System.out.println(last);
+				blast = last;
+			}
+
+			System.out.println(K);
+			bis.close();
 		}
-		System.out.println("\n&\n"+question5()+"\n");
-		System.out.println("\n&");
-		printTableInBooleanForm(endTable);
-		
-		return k;
+
+		return K;
 	}
 
 
 
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		k=0;
-		initGame();
-		endGame();
-		execute();
+		System.out.println(execute());
 	}
 
 
